f9ac6e77de1d4a818c6b634466eca9a7de3b6ac2
soonerfan-5131 <55713818+soonerfan-5131@users.noreply.github.com>
2019-10-31 12:26:25 -0500

    MesoLexicographical added

--- MesoLexicographical.java
+++ MesoLexicographical.java
@@ -1,4 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class MesoLexicographical extends MesoSortedAbstract
 {
-
+	private HashMap<String, Integer> hashMap;
+	public MesoLexicographical(HashMap<String, Integer> hash) {
+		for(String key: sortedMap(hash).keySet()) {
+			System.out.println(key);
+		}
+	}
+	@Override
+	public Map<String, Integer> sortedMap(HashMap<String, Integer> unsorted){
+		Map<String, Integer> sorted = new TreeMap<>(unsorted);
+		return sorted;
+	}
 }
